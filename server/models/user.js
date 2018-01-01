c060ee3fa05d4c6436adcaed8180bc13f7f3ec3a
AmirBenAmi <tamagotchime@gmail.com>
2018-01-01 12:56:47 +0200

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+//mongoose.plugin(schema => { schema.options.usePushEach = true });
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -30,7 +33,8 @@ var UserSchema = new mongoose.Schema({
       required: true
     }
   }]
-});
+},{usePushEach: true});
+
 
 UserSchema.methods.toJSON = function () {
   var user = this;
@@ -67,6 +71,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
